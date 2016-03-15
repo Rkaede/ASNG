678a6b36a1652068eea4b2d37f7624cdcca6f4f4
Corey M <corey.mac@gmail.com>
2016-03-15 19:28:44 +1100

    Added Ring class to karma testing

--- karma.conf.js
+++ karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       './src/ToroidalGrid.js',
+      './src/Ring.js',
       './src/Network.js',
       './src/Model.js',
       './src/Agent.js',
